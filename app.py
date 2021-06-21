c9b0c581c2f36416aeb51e995e6d4e880920c792
Anjul Rajendra <anjul.rajendra@reverieinc.com>
2021-06-21 16:30:25 +0530

    semtiment checker added for user given review

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import pickle
 from flasgger import Swagger
+import numpy as np
 
 import logging
 logger = logging.getLogger(__name__)
@@ -18,6 +19,18 @@ logger.addHandler(stream_handler)
 app = Flask(__name__)
 Swagger(app)
 
+class color:
+   PURPLE = '\033[95m'
+   CYAN = '\033[96m'
+   DARKCYAN = '\033[36m'
+   BLUE = '\033[94m'
+   GREEN = '\033[92m'
+   YELLOW = '\033[93m'
+   RED = '\033[91m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
+
 def create_sim():
     data = pd.read_csv('transformed_combined_final.csv')
     # creating a count matrix
@@ -25,7 +38,16 @@ def create_sim():
     count_matrix = cv.fit_transform(data['comb'])
     # creating a similarity score matrix
     sim = cosine_similarity(count_matrix)
-    return data,sim
+    return data, sim
+
+def get_sentiment(review):
+    reviews_list = [] # list of reviews
+    reviews_list.append(review)
+    movie_review_list = np.array([review])
+    movie_vector = vectorizer.transform(movie_review_list)
+    pred = clf.predict(movie_vector)
+    reviews_status = 'Positive' if pred else 'Negative'
+    return reviews_status
 
 def rcmd(in_movie):
     in_movie = in_movie.lower()
@@ -56,6 +78,10 @@ def home():
         in: query
         type: string
         required: true
+      - name: review_to_get_semtiment
+        in: query
+        type: string
+        required: false
     responses:
         200:
             description: The output values
@@ -63,17 +89,22 @@ def home():
     """
 
     movie_name = request.args.get('movie_name')
-    logger.info('Searched movie:{}'.format(movie_name))
-    # movie_name = 'thor'
+    req_reviews = request.args.get('review_to_get_semtiment')
+
+    logger.info(color.DARKCYAN + 'Searched movie:{}'.format(movie_name) + color.END)
+    logger.info(color.DARKCYAN + 'Review recived:{}'.format(req_reviews) + color.END)
+
     sucess, recommended_movies = rcmd(movie_name)
 
+    if req_reviews != None: review_sentiment = get_sentiment(req_reviews.lower())
+    else: review_sentiment = None
     if sucess: 
-        logger.info('recommended_movies:[{}]'.format(recommended_movies))
-        return '\n'.join(recommended_movies)
+        logger.info(color.GREEN + '\nSentiment of your Review: {}\nrecommended_movies: {}'.format(review_sentiment, recommended_movies) + color.END )
+        return 'Sentiment of your Review: {}\n\n\nrecommended_movies:\n\n{}'.format(review_sentiment, '\n'.join(recommended_movies))
     else: 
         logger.warning('{}'.format(recommended_movies))
         return recommended_movies
-        
+    
 
 
 if __name__ == '__main__':
@@ -82,13 +113,13 @@ if __name__ == '__main__':
     DEBUG = False
 
     # load the nlp model and tfidf vectorizer from disk
-    logger.info('Loading classifier model: model_nlp.pkl ')
+    logger.info(color.BLUE + 'Loading classifier model: model_nlp.pkl ' + color.END)
     filename = 'model_nlp.pkl'
     clf = pickle.load(open(filename, 'rb'))
 
-    logger.info('Loading vectorizer model: model_tranform.pkl ')
+    logger.info(color.BLUE + 'Loading vectorizer model: model_tranform.pkl ' + color.END)
     vectorizer = pickle.load(open('model_tranform.pkl','rb'))
 
-    logger.info('Server started on: http://{}:{}/apidocs/'.format(IP, PORT))
+    logger.info(color.BLUE + 'Server started on: http://{}:{}/apidocs/'.format(IP, PORT) + color.END)
     app.run(debug=True)
     
